19b3d7d915337ceff79db5f5eeb32c358060fd53
Belen Mareque <belen.mareque@gmail.com>
2020-07-28 12:48:53 -0300

    validaciones

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,7 @@
 class Booking < ApplicationRecord
-  belongs_to :experience
-  belongs_to :user
+  belongs_to :experience # experience.user == seller
+  belongs_to :user # user
+  ESTADO = %w(Proximas Completadas Canceladas)
+  validates :status, presence: true, inclusion: { in: ESTADO }
+  validates :date, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,17 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  # seller
+  has_many :experiences
+  # usuario
+  has_many :bookings
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :username, presence: true, uniqueness: true
+  validates :phone_number, presence: true
+  validates :address, presence: true
+
 end
