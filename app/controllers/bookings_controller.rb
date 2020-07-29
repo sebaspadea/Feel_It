477a1dd269b912a2f6a0eeeea859fbe0e7c6518a
pablo malczewski <malczewskipablo@gmail.com>
2020-07-29 11:59:13 -0300

    feature booking

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,34 @@
 class BookingsController < ApplicationController
+
+  def create
+    # authorize @booking
+    @user = User.find(params[:user_id])
+    @booking  = Booking.new(booking_params)
+    @booking.user = @user
+    if @booking.save
+      redirect_to user_path(@booking.user)
+    else
+      render :new
+    end
+  end
+
+   def new
+    # authorize @booking
+    @user = User.find(params[:user_id])
+    @booking = Booking.new
+  end
+
+  def destroy
+    # authorize @booking
+    @booking = Booking.find(params[:id])
+    @booking.destroy
+    redirect_to user_path(params[:user_id])
+  end
+
+  private
+
+  def booking_params
+   params.require(:booking).permit(:date, :experience_id, :user_id)
+  end
+
 end
